936a34ecb4638ed2e8de1d3b3cb32f421c806c57
Yevheniia Shakhova <yevheniia.shakhova@distributedlab.com>
2020-11-11 15:44:39 +0200

    initial commit

---
+++ src/navigation/Routes.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {Route, Switch, withRouter} from "react-router-dom";
+
+import App from "src/App"
+
+export default function Routes() {
+    return (
+        <Switch>
+            {/*<Route exact path='/' component={App}/>*/}
+        </Switch>
+    );
+}
