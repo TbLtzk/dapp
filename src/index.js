7e7611af827852a6b5c2bfc4a54fbbf518a500b2
Yevheniia Shakhova <yevheniia.shakhova@distributedlab.com>
2020-11-11 16:51:58 +0200

    add router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
 
 import {store} from './store/index';
 
@@ -11,7 +12,9 @@ import './index.css';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 );
