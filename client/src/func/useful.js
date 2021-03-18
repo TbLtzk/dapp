23721f64a63d0e0c889b975ae2255b6d65802ffe
Roman Malyshev <lem0000000000n@gmail.com>
2021-03-18 15:15:20 +0200

    Fix a bug with big number for 'Manage Rewards'

--- client/src/func/useful.js
+++ client/src/func/useful.js
@@ -59,9 +59,10 @@ export const bn = (number) => {
 };
 
 export const getPercentageFormat = (number) => {
-  return bn(1e+27)
-    .multipliedBy(number)
-    .dividedBy(100);
+  const a = new web3.utils.BN('1e+27')
+  const b = new web3.utils.BN(String(number))
+  const c = new web3.utils.BN('100')
+  return a.mul(b).div(c);
 };
 
 
