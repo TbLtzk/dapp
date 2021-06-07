ec87d04cb59ffa8578077ffe3e2d78f4c91ac40b
MrMugel <carstendrewes97@web.de>
2021-06-07 16:52:18 +0200

    Rephrased a sentence in the Q Proposal modal. Closes QDEV-2689.

--- src/pages/UserPages/Proposals/components/CreateQProposalBtn/ModalCreateProposal/CreateStep2/QProposalS2/constants.js
+++ src/pages/UserPages/Proposals/components/CreateQProposalBtn/ModalCreateProposal/CreateStep2/QProposalS2/constants.js
@@ -2,7 +2,7 @@ export const constUpdate = {
     subtitle: "Constitution Updates change the underlying agreement upon which the Q system operates.",
     radioBtnUpTitle: "Which part of the Constitution is affected",
     radioBtnUp: ["Basic Part", "Fundamental Part", "Detailed Part"],
-    inputTitle: ["Provide current constitution Hash to declare your consent", "Provide a reference link to external source"],
+    inputTitle: ["Please provide the new constitution Hash", "Provide a reference link to external source"],
     inputs: ["Hash", "External Link"],
     inputsObj: {"hash": '', "external-link": ''},
     radioBtnDownTitle: "Does Your Proposal include a Change of a Constitution Parameter?",
