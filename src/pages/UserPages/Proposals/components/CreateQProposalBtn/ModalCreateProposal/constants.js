c762b2706f86f03b345e8fce30d24dacfc133f1c
sirobaba_artem <artem.sirobaba@distributedlab.com>
2021-07-19 15:22:02 +0300

    add mode to Governance section

--- src/pages/UserPages/Proposals/components/CreateQProposalBtn/ModalCreateProposal/constants.js
+++ src/pages/UserPages/Proposals/components/CreateQProposalBtn/ModalCreateProposal/constants.js
@@ -1,4 +1,5 @@
-export const arrQProposal = ["Constitution Update", "General Q Update", "Emergency Update"];
+export const arrQProposal = ["Constitution Update", "General Q Update"];
+export const arrQProposalAdvanced = ["Constitution Update", "General Q Update", "Emergency Update"];
 export const arrQRootNode= ["Add a new Root Node", "Remove a current Root Node"];
 export const arrSlashing= ["Root Node Slashing", "Validator Node Slashing"];
 export const arrExpert= ["Add a new Expert", "Remove a current Expert", "Parameter Vote"];
