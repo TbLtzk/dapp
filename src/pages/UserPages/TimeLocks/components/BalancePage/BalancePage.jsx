8466ada5e0d80052d5a87e676bbd27a5c3d28cde
sirobaba_artem <artem.sirobaba@distributedlab.com>
2021-07-30 15:43:04 +0300

    update

--- src/pages/UserPages/TimeLocks/components/BalancePage/BalancePage.jsx
+++ src/pages/UserPages/TimeLocks/components/BalancePage/BalancePage.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import CustomBlock from 'components/Base/CustomBlock';
 import ListPaganation from './ListPaganation';
 
-function BalancePage({ title, lockAmountData, timeLockBalance, contract }) {
+function BalancePage({ title, lockAmountData }) {
     return (
         <CustomBlock>
             <h5>{title}</h5>
-            <h4>{10 + ' Q'}</h4>
+            <h4>10 Q</h4>
             <h5>Time lock balance</h5>
-            <h4>{timeLockBalance} Q</h4>
+            <h4>10 Q</h4>
             <h5>Time locks</h5>
             <ListPaganation lockAmountData={lockAmountData} />
         </CustomBlock >
