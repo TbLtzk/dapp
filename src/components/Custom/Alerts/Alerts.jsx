b5a1806d06a6b4f22271cd04537ef6d4658b64b1
sirobaba_artem <artem.sirobaba@distributedlab.com>
2021-08-16 12:06:33 +0300

    update

--- src/components/Custom/Alerts/Alerts.jsx
+++ src/components/Custom/Alerts/Alerts.jsx
@@ -1,47 +1,49 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
-import { setTransactionLoadingError } from "store/actions/action-creaters/transaction-handler";
+import { setTransactionLoadingError } from 'store/actions/action-creaters/transaction-handler'
 
-import { errorMessage } from "store/selectors/transaction-handler";
-import AlertMessage from "components/Base/AlertMessage/AlertMessage";
+import { errorMessage } from 'store/selectors/transaction-handler'
+import AlertMessage from 'components/Base/AlertMessage/AlertMessage'
 
-function Alert() {
-    const dispatch = useDispatch();
-    const [alertShow, setAlertShow] = useState(false);
+function Alert () {
+  const dispatch = useDispatch()
+  const [alertShow, setAlertShow] = useState(false)
 
-    const errorTransaction = useSelector(errorMessage);
+  const errorTransaction = useSelector(errorMessage)
 
-    const createAlert = (error) => {
-        if (error.message) {
-            const message = error.message.split(":");
-            return { title: message[0], message: message[1] };
-        }
-        if (error.status === false) {
-            return { title: "Error", message: "Not enough balance on wallet account" };
-        } else {
-            return { title: "Unknown type of error", message: "No additional info" };
-        }
-    };
+  const createAlert = (error) => {
+    if (error.message) {
+      const message = error.message.split(':')
+      return { title: message[0], message: message[1] }
+    }
+    if (error.status === false) {
+      return { title: 'Error', message: 'Not enough balance on wallet account' }
+    } else {
+      return { title: 'Unknown type of error', message: 'No additional info' }
+    }
+  }
 
-    const transactionHanlder = () => {
-        if (errorTransaction !== null) {
-            setAlertShow(true);
-        }
-    };
-    const handleAlertShow = () => {
-        setAlertShow(false);
-        dispatch(setTransactionLoadingError(null));
-    };
-    
-    useEffect(() => {
-        transactionHanlder();
-    }, [errorTransaction, dispatch]);
+  const transactionHanlder = () => {
+    if (errorTransaction !== null) {
+      setAlertShow(true)
+    }
+  }
+  const handleAlertShow = () => {
+    setAlertShow(false)
+    dispatch(setTransactionLoadingError(null))
+  }
 
-    return (
+  useEffect(() => {
+    transactionHanlder()
+  }, [errorTransaction, dispatch])
+
+  return (
         <>
-            {errorTransaction === null ? null : (
-                <div style={{ position: "absolute", right: "5%", zIndex: "50" }}>
+            {errorTransaction === null
+              ? null
+              : (
+                <div style={{ position: 'absolute', right: '5%', zIndex: '50' }}>
                     <AlertMessage
                         type="danger"
                         header={createAlert(errorTransaction).title}
@@ -50,9 +52,9 @@ function Alert() {
                         onClose={() => handleAlertShow()}
                     />
                 </div>
-            )}
+                )}
         </>
-    );
+  )
 }
 
-export default Alert;
+export default Alert
