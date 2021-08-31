ea2a552eec03ad978214bb7da7ec852b6a749bef
sirobaba_artem <artem.sirobaba@distributedlab.com>
2021-08-31 16:16:08 +0300

    fix issue

--- src/contracts/mapping/contract-to-abi.js
+++ src/contracts/mapping/contract-to-abi.js
@@ -20,7 +20,7 @@ const dynamicContracts = {
   GeneralUpdateVoting: getAbi('GeneralUpdateVoting.json'),
   LiquidationAuction: getAbi('LiquidationAuction.json'),
   QHolderRewardPool: getAbi('QHolderRewardPool.json'),
-  QVault: getAbi('QVault.json'), // removed
+  QVault: getAbi('QVault.json'),
   RootNodeRewardProxy: getAbi('RootNodeRewardProxy.json'),
   Root: getAbi('Roots.json'),
   RootsVoting: getAbi('RootsVoting.json'),
