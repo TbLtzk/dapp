e08adae75b4a3e5cc7714d09abfabdc23bb08ac4
sirobaba_artem <artem.sirobaba@distributedlab.com>
2021-09-08 17:33:38 +0300

    update

--- src/components/Custom/ConnectMetaMaskButton/ConnectMetaMaskButton.jsx
+++ src/components/Custom/ConnectMetaMaskButton/ConnectMetaMaskButton.jsx
@@ -1,37 +1,37 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
-import AlertMessage from "components/Base/AlertMessage";
-import { ButtonCustom } from "../../Base/Buttons/Button/styles";
+import AlertMessage from 'components/Base/AlertMessage'
+import { ButtonCustom } from '../../Base/Buttons/Button/styles'
 
-function ConnectMetaMaskButton(props) {
-    const { title } = props;
-    const [alertShow, setAlertShow] = useState(false);
-    const { ethereum } = window;
+function ConnectMetaMaskButton (props) {
+  const { title } = props
+  const [alertShow, setAlertShow] = useState(false)
+  const { ethereum } = window
 
-    const requestConnect = async () => {
-        try {
-            const chain = {
-                chainId: "0x8a73",
-                chainName: "TestNet",
-                rpcUrls: ["https://rpc.qtestnet.org"],
-                nativeCurrency: {
-                    name: "Q ",
-                    symbol: "Q ",
-                    decimals: 18,
-                },
-            };
-
-            await ethereum.request({
-                method: "wallet_addEthereumChain",
-                params: [chain],
-            });
-        } catch (error) {
-            console.error(error);
+  const requestConnect = async () => {
+    try {
+      const chain = {
+        chainId: '0x8a73',
+        chainName: 'TestNet',
+        rpcUrls: ['https://rpc.qtestnet.org'],
+        nativeCurrency: {
+          name: 'Q ',
+          symbol: 'Q ',
+          decimals: 18
         }
-    };
+      }
 
-    return (
+      await ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [chain]
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return (
         <>
             <ButtonCustom type="white" title={title} onClick={requestConnect}>
                 {title}
@@ -42,21 +42,21 @@ function ConnectMetaMaskButton(props) {
                 content="Install MetaMask"
                 show={alertShow}
                 onClose={() => {
-                    setAlertShow(false);
+                  setAlertShow(false)
                 }}
             />
         </>
-    );
+  )
 }
 
 ConnectMetaMaskButton.propTypes = {
-    title: PropTypes.string,
-    type: PropTypes.string,
-    handleButton: PropTypes.func,
-};
+  title: PropTypes.string,
+  type: PropTypes.string,
+  handleButton: PropTypes.func
+}
 
 ConnectMetaMaskButton.defaultProps = {
-    type: "danger",
-};
+  type: 'danger'
+}
 
-export default ConnectMetaMaskButton;
+export default ConnectMetaMaskButton
