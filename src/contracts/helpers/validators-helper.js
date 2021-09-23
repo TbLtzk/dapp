527fe00e60fbbc26c97e4951f33a17338869e6e9
sirobaba_artem <artem.sirobaba@distributedlab.com>
2021-09-23 12:22:38 +0300

    update saturation func

--- src/contracts/helpers/validators-helper.js
+++ src/contracts/helpers/validators-helper.js
@@ -2,69 +2,64 @@ import {
   getValidatorsInstance,
   getValidationRewardPoolsInstance,
   getValidatorsContract,
-  getValidatorMetricsInstance
-} from 'contracts/contract-instance'
-import { transformToPercentage } from '../handler/VotingHandler'
-import { fromWei } from 'func/balance'
-import { uintPerSecondToPerYearNumber } from 'func/useful'
-import { contractRegistryInstance } from 'contracts/contracts'
+  getValidatorMetricsInstance,
+} from "contracts/contract-instance";
+import { transformToPercentage } from "../handler/VotingHandler";
+import { fromWei } from "func/balance";
+import { uintPerSecondToPerYearNumber } from "func/useful";
+import { contractRegistryInstance } from "contracts/contracts";
 
 export const getAccountableTotalStakeFunction = async (address) => {
-  const contract = await getValidatorsContract()
-  return await contract.methods.getAccountableTotalStake(address).call()
-}
+  const contract = await getValidatorsContract();
+  return await contract.methods.getAccountableTotalStake(address).call();
+};
 
 export const getValidatorDelegatedStake = async (address) => {
-  const contract = await getValidatorsContract()
-  return await contract.methods.getValidatorDelegatedStake(address).call()
-}
+  const contract = await getValidatorsContract();
+  return await contract.methods.getValidatorDelegatedStake(address).call();
+};
 
 export const getMembersList = async () => {
-  const validatorsInstance = await getValidatorsInstance()
-  const validatorsArr = await validatorsInstance.instance.methods.getValidatorShortList().call()
-  const delegationEfficiency = await getDelegationEfficiency()
-  const delegationSaturation = await getDelegationSaturation(validatorsArr)
-  const validators = await mergeArrays(validatorsArr, delegationEfficiency, delegationSaturation)
+  const validatorsInstance = await getValidatorsInstance();
+  const validatorsArr = await validatorsInstance.instance.methods.getValidatorShortList().call();
+  const delegationSaturationAndEfficiency = await getDelegationEfficiencyAndEfficiency()
 
+  const validators = await mergeArrays(validatorsArr, delegationSaturationAndEfficiency);
   if (validatorsArr?.length === 0) {
-    return []
+    return [];
   } else {
-    return await Promise.all(validators.map((i, index) => getValidator(i, index)))
+    return await Promise.all(validators.map((i, index) => getValidator(i, index)));
   }
-}
+};
 
-const getDelegationSaturation = async (validators) => {
-  const array = Array(validators.length).fill().map((_, idx) => ({ delegationSaturation: Math.floor(Math.random() * 100) }))
-  return array
-}
-
-const mergeArrays = async (arr1, arr2, arr3) => {
+const mergeArrays = async (arr1, arr2) => {
   return arr1.map((obj, idx) => ({
     ...obj,
     ...arr2[idx],
-    ...arr3[idx]
-  }))
-}
+  }));
+};
 
-const getDelegationEfficiency = async () => {
-  const util = await getValidatorMetricsInstance()
-  await util.takeSnapshotFromNetwork(contractRegistryInstance)
-  return await util.getDelegationEfficiency()
-}
+const getDelegationEfficiencyAndEfficiency = async () => {
+  const util = await getValidatorMetricsInstance();
+  await util.takeSnapshotFromNetwork(contractRegistryInstance);
+  const efficiency = await util.getDelegationEfficiency();
+  const saturation = await util.getDelegationSaturation();
+  return await efficiency.map((item, idx) => ({ ...item, delegationSaturation: saturation[idx] }));
+};
 
 const getValidator = async (validator, index) => {
-  const validatorsInstance = await getValidatorsInstance()
-  const validationRewardPoolsInstance = await getValidationRewardPoolsInstance()
+  const validatorsInstance = await getValidatorsInstance();
+  const validationRewardPoolsInstance = await getValidationRewardPoolsInstance();
 
-  const validatorInfo = await validatorsInstance.getValidatorInfo(validator.validator)
-  const poolInfo = await validationRewardPoolsInstance.getPoolInfo(validator.validator)
+  const validatorInfo = await validatorsInstance.getValidatorInfo(validator.validator);
+  const poolInfo = await validationRewardPoolsInstance.getPoolInfo(validator.validator);
 
-  const selfStake = validatorInfo.selfStake
-  const delegatedStake = validatorInfo.delegatedStake
-  const delegatorShare = transformToPercentage(poolInfo.delegatorsShare)
-  const validatorShare = delegatorShare ? 100 - delegatorShare : 100
-  const validatorPoolBalance = fromWei(poolInfo.poolBalance)
-  const poolinterestRate = uintPerSecondToPerYearNumber(poolInfo.interestRate)
+  const selfStake = validatorInfo.selfStake;
+  const delegatedStake = validatorInfo.delegatedStake;
+  const delegatorShare = transformToPercentage(poolInfo.delegatorsShare);
+  const validatorShare = delegatorShare ? 100 - delegatorShare : 100;
+  const validatorPoolBalance = fromWei(poolInfo.poolBalance);
+  const poolinterestRate = uintPerSecondToPerYearNumber(poolInfo.interestRate);
 
   return {
     ...validator,
@@ -74,6 +69,6 @@ const getValidator = async (validator, index) => {
     delegatorShare,
     validatorShare,
     validatorPoolBalance,
-    poolinterestRate
-  }
-}
+    poolinterestRate,
+  };
+};
