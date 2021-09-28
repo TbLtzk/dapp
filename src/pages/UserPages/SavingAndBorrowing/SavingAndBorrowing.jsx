7df852b8b10ac7179d487b624c8588004f594022
MrMugel <carstendrewes97@web.de>
2021-09-28 11:51:24 +0200

    MetaMask add coin function

--- src/pages/UserPages/SavingAndBorrowing/SavingAndBorrowing.jsx
+++ src/pages/UserPages/SavingAndBorrowing/SavingAndBorrowing.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import PageWrap from 'components/Base/PageWrap'
 import Button from 'components/Base/Buttons/Button'
 import Overview from './components/Overview'
@@ -14,6 +14,7 @@ import { useSelector } from 'react-redux'
 function SavingAndBorrowing () {
   const address = useSelector(userAddressMetamask)
   const [isLoading, setIsLoading] = useState(false)
+  const { ethereum } = window
 
   const createVault = async (collateral) => {
     try {
@@ -26,6 +27,55 @@ function SavingAndBorrowing () {
       setIsLoading(false)
     }
   }
+
+  async function setAddQBTC (res) {
+    try {
+      const tokenAddress = '0x1115Ab7257e47Ef97Ea0E8254E999a8be8981952'
+      const tokenSymbol = 'QBTC'
+      const tokenDecimals = 18
+      const qbtcWasAdded = await ethereum.request({
+        method: 'wallet_watchAsset',
+        params: {
+          type: 'ERC20',
+          options: {
+            address: tokenAddress,
+            symbol: tokenSymbol,
+            decimals: tokenDecimals
+          }
+        }
+      })
+      res = qbtcWasAdded
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async function setAddQUSD (res) {
+    try {
+      const tokenAddress = '0xdC98b08363f3BfC73195dc6b532e39C51EC3c3bC'
+      const tokenSymbol = 'QUSD'
+      const tokenDecimals = 18
+      const qusdWasAdded = await ethereum.request({
+        method: 'wallet_watchAsset',
+        params: {
+          type: 'ERC20',
+          options: {
+            address: tokenAddress,
+            symbol: tokenSymbol,
+            decimals: tokenDecimals
+          }
+        }
+      })
+      res = qusdWasAdded
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    setAddQBTC()
+    setAddQUSD()
+  }, [])
 
   return (
     <PageWrap
