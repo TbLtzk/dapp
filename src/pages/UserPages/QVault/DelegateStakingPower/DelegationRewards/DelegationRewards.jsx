6ac78e1d15468c2be4330a2ed030307f51b95716
sirobaba_artem <artem.sirobaba@distributedlab.com>
2021-10-19 12:32:47 +0300

    fix bug

--- src/pages/UserPages/QVault/DelegateStakingPower/DelegationRewards/DelegationRewards.jsx
+++ src/pages/UserPages/QVault/DelegateStakingPower/DelegationRewards/DelegationRewards.jsx
@@ -1,30 +1,30 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect } from 'react'
 
-import { useDispatch, useSelector } from "react-redux";
-import { onClaimStakeDelegatorReward, getOutstandingDelegationRewards } from "store/actions/action-creaters/q-vault";
-import { outstandingDelegationRewards } from "store/selectors/q-vault";
+import { useDispatch, useSelector } from 'react-redux'
+import { onClaimStakeDelegatorReward, getOutstandingDelegationRewards } from 'store/actions/action-creaters/q-vault'
+import { outstandingDelegationRewards } from 'store/selectors/q-vault'
 
-import { fN } from "func/useful";
-import CardBlock from "components/Base/CardBlock";
+import { fN } from 'func/useful'
+import CardBlock from 'components/Base/CardBlock'
 
-export default function DelegationRewards() {
-    const dispatch = useDispatch();
-    const outstandingDelegationRewardsValue = useSelector(outstandingDelegationRewards);
+export default function DelegationRewards () {
+  const dispatch = useDispatch()
+  const outstandingDelegationRewardsValue = useSelector(outstandingDelegationRewards)
 
-    useEffect(() => {
-        dispatch(getOutstandingDelegationRewards());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getOutstandingDelegationRewards())
+  }, [dispatch])
 
-    const onClaim = useCallback(() => {
-        dispatch(onClaimStakeDelegatorReward());
-    }, [dispatch]);
+  const onClaim = useCallback(() => {
+    dispatch(onClaimStakeDelegatorReward())
+  }, [dispatch])
 
-    return (
+  return (
         <CardBlock
             title="Outstanding Delegation Rewards"
-            firstContent={fN(outstandingDelegationRewardsValue) + " Q"}
+            firstContent={fN(outstandingDelegationRewardsValue) + ' Q'}
             btnTitle="Claim Delegation Reward"
             btnHandler={onClaim}
         />
-    );
+  )
 }
