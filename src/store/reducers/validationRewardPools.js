217009721aa00d15bf57e17cc4afed6c38155018
romanmalyshev <malyshevroman.a@gmail.com>
2021-11-01 13:30:37 +0200

    Add display an appropriate warning to the Manage Staker modal

--- src/store/reducers/validationRewardPools.js
+++ src/store/reducers/validationRewardPools.js
@@ -5,7 +5,8 @@ const initialState = {
   delegatorShare: 0,
   balance: 0,
   lastUpdateOfCompoundRate: 0,
-  loadingUpdateOfCompoundRate: false
+  loadingUpdateOfCompoundRate: false,
+  isStakerRewardPoolMsgDisplayed: false
 }
 
 export default function index (state = initialState, action) {
@@ -26,6 +27,7 @@ export default function index (state = initialState, action) {
         delegatorShare: action.payload
       }
     case actionTypes.SET_VRP_LAST_UPDATE_OF_COMPOUND_RATE_DATA:
+      console.log('SET_VRP_LAST_UPDATE_OF_COMPOUND_RATE_DATA', action.payload)
       return {
         ...state,
         lastUpdateOfCompoundRate: action.payload
@@ -34,6 +36,11 @@ export default function index (state = initialState, action) {
       return {
         ...state,
         loadingUpdateOfCompoundRate: action.payload
+      }
+    case actionTypes.SET_IS_STAKER_REWARD_POOL_MSG_DISPLAYED:
+      return {
+        ...state,
+        isStakerRewardPoolMsgDisplayed: action.payload
       }
     default:
       return state
