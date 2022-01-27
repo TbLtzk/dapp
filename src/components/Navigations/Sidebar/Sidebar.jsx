9943a9450f1194e9ce69eef89fa21fa629683c39
artem_sirobaba <artem.sirobaba@distributedlab.com>
2022-01-27 15:09:56 +0200

    Run lint

--- src/components/Navigations/Sidebar/Sidebar.jsx
+++ src/components/Navigations/Sidebar/Sidebar.jsx
@@ -1,64 +1,64 @@
-import React from "react";
-import { Link, useHistory } from "react-router-dom";
+import React from 'react'
+import { Link, useHistory } from 'react-router-dom'
 
-import { useSelector } from "react-redux";
-import { qActiveProposalsCountSelector } from "store/voting/q-proposals/selectors";
-import { rootActiveProposalsCountSelector } from "store/voting/root-node-proposals/selectors";
-import { expertActiveProposalsCountSelector } from "store/voting/expert-proposals/selectors";
-import { slashingActiveProposalsCountSelector } from "store/voting/slashing-proposals/selectors";
+import { useSelector } from 'react-redux'
+import { qActiveProposalsCountSelector } from 'store/voting/q-proposals/selectors'
+import { rootActiveProposalsCountSelector } from 'store/voting/root-node-proposals/selectors'
+import { expertActiveProposalsCountSelector } from 'store/voting/expert-proposals/selectors'
+import { slashingActiveProposalsCountSelector } from 'store/voting/slashing-proposals/selectors'
 import {
-    liquidationAuctionsSelector,
-    systemDebtAuctionsSelector,
-    systemSurplusAuctionsSelector,
-} from "store/auctions/selectors";
+  liquidationAuctionsSelector,
+  systemDebtAuctionsSelector,
+  systemSurplusAuctionsSelector
+} from 'store/auctions/selectors'
 
-import LogoImg from "components/Base/LogoImg";
-import Version from "./components/Version";
+import LogoImg from 'components/Base/LogoImg'
+import Version from './components/Version'
 
-import { referencesItems } from "./constants";
+import { referencesItems } from './constants'
 
 import {
-    NavbarContainer,
-    ListContainer,
-    WrapLogo,
-    ALinkStyle,
-    LinksContainer,
-    FooterContainer,
-    Footer,
-} from "./styles";
-import { mode } from "store/dashboard-mode/selectors";
-import { MODE } from "components/Base/DashboardMode/DashboardMode";
-import CommonLinks from "./components/CommonLinks";
-import AccordionLinks from "./components/AccordionLinks";
-import DashboardMode from "components/Base/DashboardMode";
-import Themes from "components/Base/Themes";
-import AccordionElements from "./components/AccordionElements";
-import CopyAddress from "./components/CopyAddress";
-import { contractUpdatesActiveProposalsCountSelector } from "store/voting/contract-updates/selectors";
+  NavbarContainer,
+  ListContainer,
+  WrapLogo,
+  ALinkStyle,
+  LinksContainer,
+  FooterContainer,
+  Footer
+} from './styles'
+import { mode } from 'store/dashboard-mode/selectors'
+import { MODE } from 'components/Base/DashboardMode/DashboardMode'
+import CommonLinks from './components/CommonLinks'
+import AccordionLinks from './components/AccordionLinks'
+import DashboardMode from 'components/Base/DashboardMode'
+import Themes from 'components/Base/Themes'
+import AccordionElements from './components/AccordionElements'
+import CopyAddress from './components/CopyAddress'
+import { contractUpdatesActiveProposalsCountSelector } from 'store/voting/contract-updates/selectors'
 
-function Sidebar() {
-    const history = useHistory();
-    const appMode = useSelector(mode);
+function Sidebar () {
+  const history = useHistory()
+  const appMode = useSelector(mode)
 
-    const qActiveProposalsCount = useSelector(qActiveProposalsCountSelector);
-    const rootActiveProposalsCount = useSelector(rootActiveProposalsCountSelector);
-    const expertActiveProposalsCount = useSelector(expertActiveProposalsCountSelector);
-    const slashingActiveProposalsCount = useSelector(slashingActiveProposalsCountSelector);
-    const contractUpdatesActiveProposalsCount = useSelector(contractUpdatesActiveProposalsCountSelector);
+  const qActiveProposalsCount = useSelector(qActiveProposalsCountSelector)
+  const rootActiveProposalsCount = useSelector(rootActiveProposalsCountSelector)
+  const expertActiveProposalsCount = useSelector(expertActiveProposalsCountSelector)
+  const slashingActiveProposalsCount = useSelector(slashingActiveProposalsCountSelector)
+  const contractUpdatesActiveProposalsCount = useSelector(contractUpdatesActiveProposalsCountSelector)
 
-    const liquidationAuctions = useSelector(liquidationAuctionsSelector);
-    const liquidationActiveAuctionsCount = liquidationAuctions?.activeAuctions?.length;
+  const liquidationAuctions = useSelector(liquidationAuctionsSelector)
+  const liquidationActiveAuctionsCount = liquidationAuctions?.activeAuctions?.length
 
-    const systemDebtAuctions = useSelector(systemDebtAuctionsSelector);
-    const systemDebtActiveAuctionsCount = systemDebtAuctions?.activeAuctions?.length;
+  const systemDebtAuctions = useSelector(systemDebtAuctionsSelector)
+  const systemDebtActiveAuctionsCount = systemDebtAuctions?.activeAuctions?.length
 
-    const systemSurplusAuction = useSelector(systemSurplusAuctionsSelector);
-    const systemSurplusActiveAuctionsCount = systemSurplusAuction?.activeAuctions?.length;
+  const systemSurplusAuction = useSelector(systemSurplusAuctionsSelector)
+  const systemSurplusActiveAuctionsCount = systemSurplusAuction?.activeAuctions?.length
 
-    const highlight = (location) => Number(history.location.pathname === "/" + location);
-    const dashboard = <CommonLinks highlight={highlight("")} linkTo="/" linkTitle="Dashboard" />;
+  const highlight = (location) => Number(history.location.pathname === '/' + location)
+  const dashboard = <CommonLinks highlight={highlight('')} linkTo="/" linkTitle="Dashboard" />
 
-    return (
+  return (
         <header>
             <NavbarContainer expand="lg">
                 <LinksContainer>
@@ -68,23 +68,25 @@ function Sidebar() {
                         </Link>
                     </WrapLogo>
                     <ListContainer id="basic-navbar-nav">
-                        {appMode === MODE.advanced ? (
+                        {appMode === MODE.advanced
+                          ? (
                             <AccordionLinks type="dashboard-toggle" headerLink={dashboard}>
                                 <CommonLinks
-                                    highlight={highlight("monitoring")}
+                                    highlight={highlight('monitoring')}
                                     linkTo="/monitoring"
                                     linkTitle="– Monitoring"
                                 />
                             </AccordionLinks>
-                        ) : (
-                            dashboard
-                        )}
+                            )
+                          : (
+                              dashboard
+                            )}
 
                         <AccordionLinks
                             type="governance-toggle"
                             headerLink={
                                 <CommonLinks
-                                    highlight={highlight("q-governance")}
+                                    highlight={highlight('q-governance')}
                                     linkTo="/q-governance"
                                     linkTitle="Governance"
                                 />
@@ -92,71 +94,76 @@ function Sidebar() {
                         >
                             <div>
                                 <CommonLinks
-                                    highlight={highlight("q-proposals")}
+                                    highlight={highlight('q-proposals')}
                                     linkTo="/q-proposals"
                                     count={qActiveProposalsCount}
                                     linkTitle="– Q Proposals"
                                 />
 
                                 <CommonLinks
-                                    highlight={highlight("q-root-node-panel")}
+                                    highlight={highlight('q-root-node-panel')}
                                     linkTo="/q-root-node-panel"
                                     count={rootActiveProposalsCount}
                                     linkTitle="– Root Node Panel"
                                 />
 
-                                {appMode === MODE.advanced ? (
+                                {appMode === MODE.advanced
+                                  ? (
                                     <>
                                         <CommonLinks
-                                            highlight={highlight("q-expert-proposals")}
+                                            highlight={highlight('q-expert-proposals')}
                                             linkTo="/q-expert-proposals"
                                             count={expertActiveProposalsCount}
                                             linkTitle="– Expert Proposals"
                                         />
                                         <CommonLinks
-                                            highlight={highlight("slashing-proposals")}
+                                            highlight={highlight('slashing-proposals')}
                                             linkTo="/slashing-proposals"
                                             count={slashingActiveProposalsCount}
                                             linkTitle="– Slashing Proposals"
                                         />
                                         <CommonLinks
-                                            highlight={highlight("contract-updates")}
+                                            highlight={highlight('contract-updates')}
                                             linkTo="/contract-updates"
                                             count={contractUpdatesActiveProposalsCount}
                                             linkTitle="– Contract Updates"
                                         />
                                     </>
-                                ) : null}
+                                    )
+                                  : null}
                             </div>
                         </AccordionLinks>
 
-                        <CommonLinks highlight={highlight("q-vault")} linkTo="/q-vault" linkTitle="Q Vault" />
+                        <CommonLinks highlight={highlight('q-vault')} linkTo="/q-vault" linkTitle="Q Vault" />
 
-                        {appMode === MODE.advanced ? (
+                        {appMode === MODE.advanced
+                          ? (
                             <AccordionLinks
                                 type="consensus-toggle"
                                 headerLink={<CommonLinks linkTo="/root-node-staking" linkTitle="Consensus Services" />}
                             >
                                 <div>
                                     <CommonLinks
-                                        highlight={highlight("root-node-staking")}
+                                        highlight={highlight('root-node-staking')}
                                         linkTo="/root-node-staking"
                                         linkTitle="– Root Node Staking"
                                     />
                                     <CommonLinks
-                                        highlight={highlight("validator-staking")}
+                                        highlight={highlight('validator-staking')}
                                         linkTo="/validator-staking"
                                         linkTitle="– Validator Staking"
                                     />
                                 </div>
                             </AccordionLinks>
-                        ) : null}
+                            )
+                          : null}
                         <CommonLinks
-                            highlight={highlight("saving-and-borrowing")}
+                            highlight={highlight('saving-and-borrowing')}
                             linkTo="/saving-and-borrowing"
                             linkTitle="Saving & Borrowing"
                         />
-                        {appMode === MODE.advanced ? (
+                        {appMode === MODE.advanced
+                          ? (
                             <>
                                 <AccordionLinks
                                     type="auctions-toggle"
@@ -166,21 +173,21 @@ function Sidebar() {
                                 >
                                     <div>
                                         <CommonLinks
-                                            highlight={highlight("liquidation")}
+                                            highlight={highlight('liquidation')}
                                             linkTo="/liquidation"
                                             count={liquidationActiveAuctionsCount}
                                             linkTitle="– Liquidation"
                                         />
 
                                         <CommonLinks
-                                            highlight={highlight("system-debt")}
+                                            highlight={highlight('system-debt')}
                                             linkTo="/system-debt"
                                             count={systemDebtActiveAuctionsCount}
                                             linkTitle="– System Debt"
                                         />
 
                                         <CommonLinks
-                                            highlight={highlight("system-surplus")}
+                                            highlight={highlight('system-surplus')}
                                             linkTo="/system-surplus"
                                             count={systemSurplusActiveAuctionsCount}
                                             linkTitle="– System Surplus"
@@ -188,12 +195,13 @@ function Sidebar() {
                                     </div>
                                 </AccordionLinks>
                                 <CommonLinks
-                                    highlight={highlight("time-locks")}
+                                    highlight={highlight('time-locks')}
                                     linkTo="/time-locks"
                                     linkTitle="Time Locks"
                                 />
                             </>
-                        ) : null}
+                            )
+                          : null}
                     </ListContainer>
                 </LinksContainer>
 
@@ -201,7 +209,7 @@ function Sidebar() {
                     <AccordionElements margin="24px 0 0 0" title="References">
                         {referencesItems.map((value, key) => (
                             <ALinkStyle
-                                key={"references" + key}
+                                key={'references' + key}
                                 className="nav-link"
                                 href={value.location}
                                 target="_blank"
@@ -223,7 +231,7 @@ function Sidebar() {
                 </FooterContainer>
             </NavbarContainer>
         </header>
-    );
+  )
 }
 
-export default Sidebar;
+export default Sidebar
--- src/components/Navigations/Sidebar/components/AccordionLinks/AccordionLinks.jsx
+++ src/components/Navigations/Sidebar/components/AccordionLinks/AccordionLinks.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
-import { Accordion } from "react-bootstrap";
-import { AccordionIcon, LinkGroup } from "../../styles";
+import React, { useState } from 'react'
+import { Accordion } from 'react-bootstrap'
+import { AccordionIcon, LinkGroup } from '../../styles'
 
-function AccordionLinks({ children, headerLink, type }) {
-    const [isOpen, setIsOpen] = useState(localStorage.getItem(type) ? "" : "0");
+function AccordionLinks ({ children, headerLink, type }) {
+  const [isOpen, setIsOpen] = useState(localStorage.getItem(type) ? '' : '0')
 
-    const handleOpen = (state) => {
-        if (state) {
-            setIsOpen("0");
-            localStorage.setItem(type, "");
-        } else {
-            setIsOpen("");
-            localStorage.setItem(type, "0");
-        }
-    };
-    return (
-        <Accordion activeKey={isOpen ? "0" : "1"} style={{ width: "100%" }} onSelect={handleOpen}>
+  const handleOpen = (state) => {
+    if (state) {
+      setIsOpen('0')
+      localStorage.setItem(type, '')
+    } else {
+      setIsOpen('')
+      localStorage.setItem(type, '0')
+    }
+  }
+  return (
+        <Accordion activeKey={isOpen ? '0' : '1'} style={{ width: '100%' }} onSelect={handleOpen}>
             <LinkGroup>
                 {headerLink}
                 <Accordion.Toggle eventKey="0">
@@ -26,7 +26,7 @@ function AccordionLinks({ children, headerLink, type }) {
             </LinkGroup>
             <Accordion.Collapse eventKey="0">{children}</Accordion.Collapse>
         </Accordion>
-    );
+  )
 }
 
-export default AccordionLinks;
+export default AccordionLinks
