06960368ca4f7ddb64d781d1d1753a5c636199d4
artem_sirobaba <artem.sirobaba@distributedlab.com>
2022-02-02 14:34:25 +0200

    Remove balance fix

--- src/store/system-balance/sagas.js
+++ src/store/system-balance/sagas.js
@@ -1,5 +1,5 @@
-import { put, select, takeEvery, call } from 'redux-saga/effects'
-import * as actionTypes from './action-types'
+import { put, select, takeEvery, call } from "redux-saga/effects";
+import * as actionTypes from "./action-types";
 import {
   getDebtError,
   getDebtSuccess,
@@ -11,60 +11,59 @@ import {
   onPerformNettingError,
   getSystemBalance,
   getDebt,
-  getSurplus
-} from './action-creators'
-import { getSystemBalanceInstance } from 'contracts/contract-instance'
-import ErrorHandler from 'func/ErrorHandler'
-import { fromWei } from 'func/balance'
-import { setErrorMessage } from 'store/transaction-handler/action-creators'
+  getSurplus,
+} from "./action-creators";
+import { getSystemBalanceInstance } from "contracts/contract-instance";
+import ErrorHandler from "func/ErrorHandler";
+import { fromWei } from "func/balance";
+import { setErrorMessage } from "store/transaction-handler/action-creators";
 
-function * getSurplusGenerator () {
+function* getSurplusGenerator() {
   try {
-    const contract = yield call(getSystemBalanceInstance)
-    const data = yield contract.getSurplus()
-    yield put(getSurplusSuccess(fromWei(data)))
+    const contract = yield call(getSystemBalanceInstance);
+    const data = yield contract.getSurplus();
+    yield put(getSurplusSuccess(fromWei(data)));
   } catch (error) {
-    ErrorHandler.processWithoutFeedback(error)
-    yield put(getSurplusError(0))
+    ErrorHandler.processWithoutFeedback(error);
+    yield put(getSurplusError(0));
   }
 }
 
-function * getDebtGenerator () {
+function* getDebtGenerator() {
   try {
-    const contract = yield call(getSystemBalanceInstance)
-    const data = yield contract.getDebt()
-    yield put(getDebtSuccess(fromWei(data)))
+    const contract = yield call(getSystemBalanceInstance);
+    const data = yield contract.getDebt();
+    yield put(getDebtSuccess(fromWei(data)));
   } catch (error) {
-    ErrorHandler.processWithoutFeedback(error)
-    yield put(getDebtError(0))
+    ErrorHandler.processWithoutFeedback(error);
+    yield put(getDebtError(0));
   }
 }
 
-function * getSystemBalanceGenerator () {
+function* getSystemBalanceGenerator() {
   try {
-    const contract = yield call(getSystemBalanceInstance)
-    const data = yield contract.getBalance()
-    yield put(getSystemBalanceSuccess(fromWei(data)))
-    yield put(getSystemBalanceSuccess(0))
+    const contract = yield call(getSystemBalanceInstance);
+    const data = yield contract.getBalance();
+    yield put(getSystemBalanceSuccess(fromWei(data)));
   } catch (error) {
-    ErrorHandler.processWithoutFeedback(error)
-    yield put(getSystemBalanceError(0))
+    ErrorHandler.processWithoutFeedback(error);
+    yield put(getSystemBalanceError(0));
   }
 }
 
-function * onPerformNettingGenerator () {
+function* onPerformNettingGenerator() {
   try {
-    const { userAddress } = yield select((state) => state.userInf)
-    const contract = yield call(getSystemBalanceInstance)
-    const data = yield contract.performNetting({ from: userAddress })
-    yield put(onPerformNettingSuccess(data))
-    yield put(getSystemBalance())
-    yield put(getDebt())
-    yield put(getSurplus())
+    const { userAddress } = yield select((state) => state.userInf);
+    const contract = yield call(getSystemBalanceInstance);
+    const data = yield contract.performNetting({ from: userAddress });
+    yield put(onPerformNettingSuccess(data));
+    yield put(getSystemBalance());
+    yield put(getDebt());
+    yield put(getSurplus());
   } catch (error) {
-    const errorMsg = ErrorHandler.process(error)
-    yield put(setErrorMessage(errorMsg))
-    yield put(onPerformNettingError(error))
+    const errorMsg = ErrorHandler.process(error);
+    yield put(setErrorMessage(errorMsg));
+    yield put(onPerformNettingError(error));
   }
 }
 
@@ -72,5 +71,5 @@ export default [
   takeEvery(actionTypes.GET_SURPLUS, getSurplusGenerator),
   takeEvery(actionTypes.GET_DEBT, getDebtGenerator),
   takeEvery(actionTypes.GET_SYSTEM_BALANCE, getSystemBalanceGenerator),
-  takeEvery(actionTypes.ON_PERFORM_NETTING, onPerformNettingGenerator)
-]
+  takeEvery(actionTypes.ON_PERFORM_NETTING, onPerformNettingGenerator),
+];
