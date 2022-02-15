efe7e41209ec97df0579558f6c11ecc9b0580c83
artem_sirobaba <artem.sirobaba@distributedlab.com>
2022-02-15 11:21:59 +0200

    Update styles in auctions tab

--- src/pages/UserPages/Auctions/styles.js
+++ src/pages/UserPages/Auctions/styles.js
@@ -1,11 +1,11 @@
-import styled from "styled-components";
-import { indents } from "constants/style";
+import styled from 'styled-components'
+import { indents } from 'constants/style'
 
 export const AuctionsTabWrp = styled.div`
   display: grid;
   grid-template-columns: minmax(100px, 2fr) minmax(100px, 1fr);
-  grid-column-gap: ${indents["15"]};
-`;
+  grid-column-gap: ${indents['15']};
+`
 
 export const AuctionCardBodyContainer = styled.div`
   .auction-card_elements {
@@ -27,4 +27,4 @@ export const AuctionCardBodyContainer = styled.div`
       }
     }
   }
-`;
+`
