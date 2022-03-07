954d8c781ec60aeafc2cd30c349e116137ed0d20
artem_sirobaba <artem.sirobaba@distributedlab.com>
2022-03-07 16:14:31 +0200

    Fix layout

--- src/components/Custom/LoadingMetaMask/styles.js
+++ src/components/Custom/LoadingMetaMask/styles.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components'
 
 export const WrapContainer = styled.div`
-  background-color: #07172b;
-  color: #ffffff;
-  display: flex;
+  background-color: ${(props) => props.theme.colors.oxfordBlue};
+  color: ${(props) => props.theme.colors.white};
   flex-direction: ${(p) => p.direction};
+  display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  margin-left: auto;
+  margin-right: auto;
+  height: calc(100vh - 70px);
 `
