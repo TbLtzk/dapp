0d939f0cc3102bde2b13f2df29921fe9d1c74995
artem_sirobaba <artem.sirobaba@distributedlab.com>
2022-05-19 12:00:51 +0300

    Update

--- src/constants/config.js
+++ src/constants/config.js
@@ -89,7 +89,7 @@ export const URLS = {
     indexer: indexersUrls.devnet,
     explorer: explorerUrls.devnet,
   },
-  'http://63.34.190.209:8000/': {
+  'http://63.34.190.209:8000': {
     id: 'testnet',
     name: 'Testnet',
     chainId: 35443,
@@ -98,7 +98,7 @@ export const URLS = {
     indexer: indexersUrls.testnet,
     explorer: explorerUrls.testnet,
   },
-  'http://35.161.73.158:8000/': {
+  'http://35.161.73.158:8000': {
     id: 'testnet',
     name: 'Testnet',
     chainId: 35443,
