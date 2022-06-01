fc21de2e2990f73f4829d0b268dda5fae8276bfc
Maksym Shopynskyi <ardier16@gmail.com>
2022-06-01 15:15:36 +0000

    [QDEV-3901, QDEV-4051, QDEV-4056] Root node proposal form fixes

--- src/pages/Proposals/components/CreateProposal/RootProposalModal/components/ConfirmStep/ConfirmStep.jsx
+++ src/pages/Proposals/components/CreateProposal/RootProposalModal/components/ConfirmStep/ConfirmStep.jsx
@@ -4,6 +4,8 @@ import ModalStep from 'components/Base/ModalStep';
 
 import { useCreateProposal } from '../../RootProposalModal';
 
+import { CONTRACT_TYPES } from 'constants/contracts';
+
 function ConfirmStep () {
   const { values, confirm, goBack } = useCreateProposal();
 
@@ -17,13 +19,17 @@ function ConfirmStep () {
       <h5>Type:</h5>
       <p className="text-capitalize">{values.type.replace(/-/g, ' ')}</p>
 
-      <h5>Hash:</h5>
-      <p>{values.hash}</p>
+      {values.type === CONTRACT_TYPES.addAnewRootNode && (
+        <>
+          <h5>Hash:</h5>
+          <p>{values.hash}</p>
 
-      <h5>Remove a Current Root Node:</h5>
-      <p className="text-capitalize">{values.isRemovingNode ? 'yes' : 'no'}</p>
+          <h5>Remove a Current Root Node:</h5>
+          <p className="text-capitalize">{values.isRemovingNode ? 'yes' : 'no'}</p>
+        </>
+      )}
 
-      {values.isRemovingNode && (
+      {(values.isRemovingNode || values.type === CONTRACT_TYPES.removeACurrentRootNode) && (
         <>
           <h5>Root Node to Remove:</h5>
           <p>{values.address}</p>
