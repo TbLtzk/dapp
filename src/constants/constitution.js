0d7b5df93368c9e251a2fe954bd43e9c1b52e945
Oleksii Yurchenko <ahelless@zohomail.eu>
2022-06-02 09:24:26 +0000

    Update constitution ip

--- src/constants/constitution.js
+++ src/constants/constitution.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'http://54.214.137.92:8999/';
+export const baseUrl = 'http://34.248.83.162:8999/';
 export const latestConstitution = `${baseUrl}constitution/latest`;
 export const archiveConstitution = baseUrl;
