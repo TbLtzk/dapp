b1aae03cdeef516d52cf04e50ab7196f8a016f52
artem_sirobaba <artem.sirobaba@distributedlab.com>
2022-06-10 13:02:05 +0300

    Change constitution link

--- src/constants/constitution.js
+++ src/constants/constitution.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'http://34.248.83.162:8999/';
+export const baseUrl = 'https://constitution.q.org/';
 export const latestConstitution = `${baseUrl}constitution/latest`;
 export const archiveConstitution = baseUrl;
