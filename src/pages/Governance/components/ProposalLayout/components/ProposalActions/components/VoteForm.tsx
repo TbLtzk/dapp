944562b69fb9d4610d9c8c0f38ce474acb8d0d06
Oleh Tryfonov <oleg.himik.trifon@gmail.com>
2022-09-01 09:26:40 +0000

    add voting weight on modal

--- src/pages/Governance/components/ProposalLayout/components/ProposalActions/components/VoteForm.tsx
+++ src/pages/Governance/components/ProposalLayout/components/ProposalActions/components/VoteForm.tsx
@@ -1,8 +1,9 @@
 
 import { useTranslation } from 'react-i18next';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Proposal } from 'typings/proposals';
+import { fromWei } from 'web3-utils';
 
 import Button from 'ui/Button';
 import RadioGroup from 'ui/RadioGroup';
@@ -12,16 +13,23 @@ import useForm from 'hooks/useForm';
 import { StyledVoteForm } from './styles';
 
 import { voteForProposal } from 'store/voting/proposals/actions';
+import { baseVotingWeightInfoSelector } from 'store/voting/proposals/selectors';
 
+import { formatAsset } from 'utils/numbers';
 import { required } from 'utils/validators';
 
 interface Props {
   proposal: Proposal
+  isMemberVoting?: boolean;
 }
 
-function VoteForm ({ proposal }: Props) {
+function VoteForm ({ proposal, isMemberVoting }: Props) {
   const { t } = useTranslation();
   const dispatch = useDispatch();
+
+  const { ownWeight } = useSelector(baseVotingWeightInfoSelector);
+
+  const weight = formatAsset(fromWei(ownWeight || '0'), 'Q');
 
   const form = useForm({
     initialValues: { vote: '' },
@@ -43,6 +51,17 @@ function VoteForm ({ proposal }: Props) {
       $selectedOption={form.values.vote === 'yes' ? 'for' : 'against'}
       onSubmit={form.submit}
     >
+      {!isMemberVoting &&
+        <div>
+          <p className="text-md">{t('TOTAL_VOTING_WEIGHT')}</p>
+          <p
+            className="text-xl font-semibold"
+            title={weight}
+          >
+            {weight}
+          </p>
+        </div>
+      }
       <RadioGroup
         {...form.fields.vote}
         extended
