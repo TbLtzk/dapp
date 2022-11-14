b9db28bc2f74f3403f7a5840baaa9a79273f28b1
Maksym Shopynskyi <ardier16@gmail.com>
2022-11-14 15:30:21 +0000

    [QDEV-4980] Fix contract upgrade proposal voted check

--- src/contracts/helpers/voting/index.ts
+++ src/contracts/helpers/voting/index.ts
@@ -215,7 +215,9 @@ export async function getProposal<T extends ProposalContractType> (
     const stats = (await contract.getProposalStats(id)) as VotingStats;
     const parameters = 'getParametersArr' in contract ? await contract.getParametersArr(id) : [];
 
-    const userVoted = 'hasUserVoted' in contract ? await contract.hasUserVoted(id, userAddress) : false;
+    const userVoted = 'hasUserVoted' in contract
+      ? await contract.hasUserVoted(id, userAddress)
+      : await contract.instance.methods.voted(id, userAddress);
     const userVetoed = 'hasRootVetoed' in contract ? await contract.hasRootVetoed(id, userAddress) : false;
 
     const rootNodesInstance = await getRootNodesInstance();
