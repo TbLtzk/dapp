ffce326a6b4a4a8865513c6d5d37fba58d9265aa
Oleh Tryfonov <oleg.himik.trifon@gmail.com>
2023-01-25 17:54:33 +0000

    [QDEV-4925] Fix closing vote modal

--- src/pages/Governance/components/ProposalLayout/components/ProposalActions/components/VoteForm.tsx
+++ src/pages/Governance/components/ProposalLayout/components/ProposalActions/components/VoteForm.tsx
@@ -37,7 +37,7 @@ function VoteForm ({ proposal, isMemberVoting, onSubmit }: Props) {
     onSubmit: (form) => {
       submitTransaction({
         successMessage: t('VOTE_SUCCESS'),
-        onSuccess: () => onSubmit(),
+        onConfirm: () => onSubmit(),
         submitFn: () => voteForProposal({
           type: 'basic',
           isVotedFor: form.vote === 'yes',
