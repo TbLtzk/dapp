12a1d543835f678c83be38ec4a63b11ca236093d
Maksym Shopynskyi <ardier16@gmail.com>
2023-04-26 09:08:31 +0000

    [QDEV-5946] Add redirects for old governance links

--- src/navigation/Routes.tsx
+++ src/navigation/Routes.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense, useEffect } from 'react';
-import { Route, RouteComponentProps, Switch } from 'react-router-dom';
+import { Redirect, Route, RouteComponentProps, Switch } from 'react-router-dom';
 
 import * as Sentry from '@sentry/react';
 import { ProposalContractType } from 'typings/contracts';
@@ -82,7 +82,7 @@ function Routes () {
 
         <Route
           exact
-          path={RoutePaths.proposal}
+          path={[RoutePaths.proposal, '/q-governance/proposal/:contract?/:id?']}
           component={(props: RouteComponentProps<{ id: string; contract: ProposalContractType }>) => (
             <Proposal {...props} />
           )}
@@ -169,6 +169,27 @@ function Routes () {
             </Suspense>
           )}
         />
+
+        {/* Old governance routes for backward compatibility */}
+        <Route exact path="/q-proposals">
+          <Redirect to={RoutePaths.qProposals} />
+        </Route>
+
+        <Route exact path="/q-root-node-panel">
+          <Redirect to={RoutePaths.rootNodePanel} />
+        </Route>
+
+        <Route exact path="/q-expert-proposals">
+          <Redirect to={RoutePaths.expertProposals} />
+        </Route>
+
+        <Route exact path="/slashing-proposals">
+          <Redirect to={RoutePaths.slashingProposals} />
+        </Route>
+
+        <Route exact path="/contract-updates">
+          <Redirect to={RoutePaths.contractUpdates} />
+        </Route>
 
         <Route component={NotFound} />
       </Switch>
